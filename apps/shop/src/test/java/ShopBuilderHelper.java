9804ef4ae5d2506a726e552449de5dbb476701bf
ryan <ryan.taplin170596@gmail.com>
2017-08-05 23:14:27 +0100

    remove unnecessary shop tests and add ShopBuilderHelper

---
+++ apps/shop/src/test/java/ShopBuilderHelper.java
@@ -0,0 +1,49 @@
+import time.ShopTime;
+import time.SimpleShopTime;
+import time.digit.ShopHour;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ShopBuilderHelper {
+    // SHOP ITEMS
+    private static final Till till = new Till();
+    private static final Staff staff = new Staff();
+
+    // DEFAULT SHOP
+    public static final int NO_OF_DEFAULT_TILLS = 5;
+    private static final List<Till> DEFAULT_TILLS = new ArrayList<>(Arrays.asList(till, till, till, till, till));
+
+    public static final int NO_OF_DEFAULT_STAFF = 12;
+    private static final List<Staff> DEFAULT_STAFF = new ArrayList<>(Arrays.asList(staff, staff, staff, staff, staff, staff, staff, staff, staff, staff, staff, staff));
+
+    public static final ShopTime DEFAULT_OPENING = new SimpleShopTime().withHour(new ShopHour(9));
+    public static final ShopTime DEFAULT_CLOSING = new SimpleShopTime().withHour(new ShopHour(18));
+    public static final Shop EXPECTED_DEFAULT_SHOP = new Shop(DEFAULT_OPENING.value(), DEFAULT_CLOSING.value(), DEFAULT_TILLS, DEFAULT_STAFF);
+
+
+    // CUSTOM SHOP
+    public static final int NO_OF_CUSTOM_TILLS = 1;
+    private static final List<Till> CUSTOM_TILLS = new ArrayList<>(Arrays.asList(till));
+
+    public static final int NO_OF_CUSTOM_STAFF = 2;
+    private static final List<Staff> CUSTOM_STAFF = new ArrayList<>(Arrays.asList(staff, staff));
+
+    public static final ShopTime CUSTOM_OPENING = new SimpleShopTime().withHour(new ShopHour(0));
+    public static final ShopTime CUSTOM_CLOSING = new SimpleShopTime().withHour(new ShopHour(23));
+    public static final Shop EXPECTED_CUSTOM_SHOP = new Shop(CUSTOM_OPENING.value(), CUSTOM_CLOSING.value(), CUSTOM_TILLS, CUSTOM_STAFF);
+
+    public Shop getDefaultShop() {
+        return new ShopBuilder().build();
+    }
+
+    public Shop getCustomShop() {
+        return new ShopBuilder()
+                .withOpeningTime(CUSTOM_OPENING)
+                .withClosingTime(CUSTOM_CLOSING)
+                .withStaff(NO_OF_CUSTOM_STAFF)
+                .withTills(NO_OF_CUSTOM_TILLS)
+                .build();
+    }
+}
--- apps/shop/src/test/java/ShopTest.java
+++ apps/shop/src/test/java/ShopTest.java
@@ -1,61 +1,14 @@
 import org.junit.Before;
 import org.junit.Test;
-import time.SimpleShopTime;
-import time.digit.ShopHour;
-import time.digit.ShopMinute;
-import time.digit.ShopSecond;
-
-import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ShopTest {
+public class ShopTest extends ShopBuilderHelper {
 
-    private ShopBuilder shopBuilder;
     private Shop shop;
 
-    private static final Date DEFAULT_CLOSING = new SimpleShopTime().withHour(new ShopHour(18)).value();
-    private static final Date DEFAULT_OPENING = new SimpleShopTime().withHour(new ShopHour(9)).value();
-
-    private static final ShopHour CUSTOM_MIN_HOUR = new ShopHour(0);
-    private static final ShopMinute CUSTOM_MIN_MINUTE = new ShopMinute(0);
-    private static final ShopSecond CUSTOM_MIN_SECOND = new ShopSecond(0);
-
-    private static final SimpleShopTime CUSTOM_OPENING = new SimpleShopTime()
-            .withHour(CUSTOM_MIN_HOUR).withMinute(CUSTOM_MIN_MINUTE).withSecond(CUSTOM_MIN_SECOND);
-
-    private static final ShopHour CUSTOM_MAX_HOUR = new ShopHour(23);
-    private static final ShopMinute CUSTOM_MAX_MINUTE = new ShopMinute(59);
-    private static final ShopSecond CUSTOM_MAX_SECOND = new ShopSecond(59);
-
-    private static final SimpleShopTime CUSTOM_CLOSING = new SimpleShopTime()
-            .withHour(CUSTOM_MAX_HOUR).withMinute(CUSTOM_MAX_MINUTE).withSecond(CUSTOM_MAX_SECOND);
-
-
-    @Before
-    public void setUp() throws Exception {
-        shopBuilder = new ShopBuilder();
-    }
-
     @Test
-    public void shopHasDefaultHoursOf9hrTo18hr() throws Exception {
-        givenDefaultShop();
-        assertThat(shop.openingHour).isEqualTo(DEFAULT_OPENING);
-        assertThat(shop.closingHour).isEqualTo(DEFAULT_CLOSING);
-    }
+    public void name() throws Exception {
 
-    @Test
-    public void shopCanHaveCustomHoursOf0To23() throws Exception {
-        givenCustomShop();
-        assertThat(shop.openingHour).isEqualTo(CUSTOM_OPENING);
-        assertThat(shop.closingHour).isEqualTo(CUSTOM_CLOSING);
-    }
-
-    private void givenCustomShop() {
-        shop = shopBuilder.withOpeningTime(CUSTOM_OPENING).withClosingTime(CUSTOM_CLOSING).build();
-    }
-
-    private void givenDefaultShop() {
-        shop = shopBuilder.build();
     }
 }
